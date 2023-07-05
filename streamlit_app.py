83aae2e9102577741de64be010674a59efba4953
routhmahesh <routhmahesh1@gmail.com>
2023-07-05 09:39:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ except URLError as e:
 
 # write your own comment - what does this do?
 
-streamlit.stop()
+#streamlit.stop()
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
